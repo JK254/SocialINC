227bcf57d191706876e8bcbd56414639e5edfe41
Vongdara <brittny.vongdara@statcan.gc.ca>
2022-07-28 13:17:51 -0400

    changed the exporting process so it can all be run in one go

--- 1_collect_data.R
+++ 1_collect_data.R
@@ -1,3 +1,22 @@
+# Install packages ----
+for (i in c(
+  "shiny",
+  "tidyverse",
+  "data.table",
+  "readxl",
+  "plotly",
+  "leaflet",
+  "htmltools",
+  "RColorBrewer",
+  "shinyjs",
+  "cansim"
+)) {
+  if (!i %in% installed.packages()) {
+    install.packages(i, dependencies = TRUE)
+  }
+  require(i)
+}
+
 # Load packages ----
 library(tidyverse) # for dplyr
 library(cansim) # for retrieving CODR tables
@@ -6,8 +25,8 @@ library(arrow) # to write files to parquet (smaller than .csv files)
 #'NOTE [You might need to run each data pull one by one otherwise it might crash your session since it's a lot of data. However, once you've done pulled the data you don't need to do it again since it looks like the data is periodically collected and disseminated.]
 
 # Functions ----
-#'NOTE [you need to specific what variable grouping you want to pull and what CODR table you want. E.g., if you want just REF_DATE, GEO, `Visible minority`, `Selected sociodemographic characteristics`, Indicators, and VALUE you'd only need to reference the "base" pull]
-func_codr <- function(pull_type, codr_no) {
+#'NOTE [you need to specific: 1. what CODR table you want, and 2. what you want to name this file, and 3. what variable grouping you want to pull (E.g., if you want just REF_DATE, GEO, `Visible minority`, `Selected sociodemographic characteristics`, Indicators, and VALUE you'd only need to reference the "base" pull)]
+func_codr <- function(codr_no, df_name, pull_type) {
   if (pull_type == "base") {
     #'NOTE [Return reference period, geography, visible minority, characteristic, indicator, and value as a base retrieval]
     df <- 
@@ -21,7 +40,7 @@ func_codr <- function(pull_type, codr_no) {
         Value = VALUE
       ) # select and rename the relevant variables
   } else if (pull_type == "condfidence") {
-    #'NOTE [Retrieve confidence/characteristics on top of base retrieval]
+    #'NOTE [Retrieve confidence/**characteristics** on top of base retrieval]
     df <- 
       get_cansim(codr_no) %>%
       select(
@@ -34,7 +53,7 @@ func_codr <- function(pull_type, codr_no) {
         Value = VALUE
       ) # select and rename the relevant variables
   } else if (pull_type == "characteristics") {
-    #'NOTE [Retrieve confidence/statistics on top of base retrieval]
+    #'NOTE [Retrieve confidence/**statistics** on top of base retrieval]
     df <- 
       get_cansim(codr_no) %>%
       select(
@@ -47,14 +66,17 @@ func_codr <- function(pull_type, codr_no) {
         Value = VALUE
       ) # select and rename the relevant variables
   }
+  
+  write_parquet(x = df, sink = paste0("./_tempdata/", df_name,".parquet"))
+  
   gc()
-  return(df)
 }
 
+#'NOTE [you need to specific 1. what CODR table you want and 2. what you want to name this file]
 #'NOTE [Return first official language spoken (on top of base variables)]
 #'NOTE [These tables are huge and require the get_cansim_sqlite function instead of the regular get_cansim function, otherwise we run out of memory]
-func_sql <- function(x) {
-  conn <- get_cansim_sqlite(x,
+func_sql <- function(codr_no, df_name) {
+  conn <- get_cansim_sqlite(codr_no,
                             refresh = TRUE,
                             timeout = 1000,
                             language = "en")
@@ -75,79 +97,80 @@ func_sql <- function(x) {
     ) # select and rename the relevant variables
   
   gc() # this clears up the memory so we can continute to retrieve more data
+  disconnect_cansim_sqlite(conn) # this disconnects the SQL connection
+  remove_cansim_sqlite_cached_table(cansimTableNumber = codr_no) # this removes cached data
+  write_parquet(x = df, sink = paste0("./_tempdata/", df_name, ".parquet")) # this exports the dataframe into a parquet file
   
-  disconnect_cansim_sqlite(conn)
-  
-  remove_cansim_sqlite_cached_table(cansimTableNumber = x)
-  
-  return(df)
 }
 
 # Data loading and pre-processing ----
 ## Selected labour force status ----
-rateDT <-
-  func_sql("43-10-0069")
+func_sql(codr_no = "43-10-0069", df_name = "rateDT")
 
 ## Average employment income indicators ----
-incomeDT <-
-  func_sql("43-10-0068")
+func_sql(codr_no = "43-10-0068", df_name = "incomeDT")
 
 ## Basic needs and housing data ----
-basicDT <- 
-  func_codr(pull_type = "condfidence", codr_no = "13-10-0841")
+func_codr(codr_no = "13-10-0841",
+          df_name = "basicDT",
+          pull_type = "condfidence")
 
 ## Health indicators ----
-healthDT <- 
-  basicDT 
 #'NOTE [Q: is this supposed to be a copy of the other table? A: yes basic and health are almost similar but one has expounded geography]
+func_codr(codr_no = "13-10-0841",
+          df_name = "healthDT",
+          pull_type = "condfidence")
 
-## Civic engagement data ----
-civicDT <- 
-  func_codr(pull_type = "characteristics", codr_no = "43-10-0065")
+## Civic engagement ----
+func_codr(codr_no = "43-10-0065",
+          df_name = "civicDT",
+          pull_type = "characteristics")
 
 ### Civic engagement and engagement in political activities ----
-civicDT2 <- 
-  func_codr(pull_type = "characteristics", codr_no = "43-10-0066")
+func_codr(codr_no = "43-10-0066",
+          df_name = "civicDT2",
+          pull_type = "characteristics")
 
 ## Representation (Selected management occupations and self-employed class of worker) ----
-representationDT <-
-  func_sql("43-10-0070")
-
-remove_cansim_sqlite_cached_table(cansimTableNumber = "43-10-0070")
+func_sql(codr_no = "43-10-0070", df_name = "representationDT")
 
 ## Youth not in employment ----
-youthDT <- 
-  get_cansim("43-10-0072") %>%
+get_cansim("43-10-0072") %>%
   select(
     Year = REF_DATE,
     Geography = GEO,
     Sex,
     Age = `Age group`,
     Language = `First official language spoken`,
-    Immigration = `Generation status`, 
+    Immigration = `Generation status`,
     # This variable ^ isn't found in the other tables hence no function for this CODR
     VisMin = `Visible minority status`,
     Indicator = Indicators,
     Value = VALUE
-  ) # select and rename the relevant variables
+  ) %>% # select and rename the relevant variables
+  write_parquet(sink = paste0("./_tempdata/youthDT.parquet"))
 gc()
 
 ## Sense of belonging ----
-belongingDT <-
-  func_codr(pull_type = "characteristics", codr_no = "43-10-0064")
+func_codr(codr_no = "43-10-0064",
+          df_name = "belongingDT",
+          pull_type = "characteristics")
 
 #'NOTE [this table has not been released yet / 2022-07-04]
-# ## Employment data ----
-# employmentDT <- 
-#   func_codr(pull_type = "base", codr_no = "43-10-0059")
+# ## Employment ----
+# func_codr(codr_no = "43-10-0059",
+#           df_name = "employmentDT",
+#           pull_type = "base")
 
 ## Confidence in Canadian institutions ----
-confidenceDT <-
-  func_codr(pull_type = "characteristics", codr_no = "43-10-0062")
+func_codr(codr_no = "43-10-0062",
+          df_name = "confidenceDT", 
+          pull_type = "characteristics")
 
 ## Discrimination ----
-discriminationDT <-
-  func_codr(pull_type = "characteristics", y = "43-10-0061")
+func_codr(codr_no = "43-10-0061",
+          df_name = "discriminationDT",
+          pull_type = "characteristics")
 
 ## Education ----
 education_filter <-
@@ -169,7 +192,7 @@ education_filter <-
     "60", # Northwest Territories
     "61", # Yukon
     "62" # Nunavut
-  ) # can only retrieve at the provincial level because otherwise it's costs too much memory
+  ) # can only retrieve at the provincial level because otherwise it costs too much memory
 
 conn <-
   get_cansim_sqlite(
@@ -195,6 +218,7 @@ educationDT_prov <-
     Indicator = Indicators,
     Value = VALUE
   ) # select and rename the relevant variables
+
 gc()
 
 educationDT_cma <- 
@@ -213,14 +237,13 @@ educationDT_cma <-
     Indicator = Indicators,
     Value = VALUE
   ) # select and rename the relevant variables
+
 gc()
-
 disconnect_cansim_sqlite(conn)
-
 remove_cansim_sqlite_cached_table(cansimTableNumber = "43-10-0067")
 
-educationDT <-
-  rbind(educationDT_prov, educationDT_cma)
+bind_rows(educationDT_prov, educationDT_cma) %>%
+  write_parquet(sink = paste0("./_tempdata/educationDT.parquet"))
 
 rm(educationDT_prov, educationDT_cma)
 
@@ -254,6 +277,7 @@ OverQualDT_canada <-
     Indicator = Indicators,
     Value = VALUE
   ) # select and rename the relevant variables
+
 gc()
 
 ### Regions ----
@@ -284,6 +308,7 @@ OverQualDT_region <-
     Indicator = Indicators,
     Value = VALUE
   ) # select and rename the relevant variables
+
 gc()
 
 ### Provinces & territories ----
@@ -320,12 +345,13 @@ OverQualDT_prov <-
     Indicator = Indicators,
     Value = VALUE
   ) # select and rename the relevant variables
+
 gc()
 
-OverQualDT <-
-  rbind(OverQualDT_canada,
-        OverQualDT_region,
-        OverQualDT_prov)
+bind_rows(OverQualDT_canada,
+          OverQualDT_region,
+          OverQualDT_prov) %>%
+  write_parquet(sink = paste0("./_tempdata/OverQualDT.parquet"))
 
 rm(OverQualDT_canada,
    OverQualDT_region,
@@ -452,17 +478,12 @@ gc()
 remove_cansim_sqlite_cached_table(cansimTableNumber = "43-10-0071")
 disconnect_cansim_sqlite(conn)
 
-#'NOTE [it would be ideally to run this to have everything related to overqualification together but this whole process takes a really long time/it crashes so to be safe I'm outputting two data files which I'll combine later]
-# OverQualDT <-
-#   OverQualDT %>%
-#   bind_rows(OverQualDT_cma_1,
-#             OverQualDT_cma_2,
-#             OverQualDT_cma_3)
+# FIXME: it would be ideally to run this to have everything related to overqualification together but this whole process takes a really long time/it crashes so to be safe I'm outputting two data files which I'll combine later
 
-OverQualDT_cma <-
-  bind_rows(OverQualDT_cma_1,
+bind_rows(OverQualDT_cma_1,
             OverQualDT_cma_2,
-            OverQualDT_cma_3)
+            OverQualDT_cma_3) %>%
+  write_parquet(sink = paste0("./_tempdata/OverQualDT_cma.parquet"))
 gc()
 
 rm(conn,
@@ -471,25 +492,12 @@ rm(conn,
    OverQualDT_cma_3)
 
 ## Police-reported hate crime ----
-polData <-
-  get_cansim("35-10-0066") %>%
+get_cansim("35-10-0066") %>%
   select(
     Year = REF_DATE,
     Geography = GEO,
     Motivation = `Type of motivation`,
     Value = VALUE
-  ) # select and rename the relevant variables
+  ) %>% # select and rename the relevant variables
+  write_parquet(sink = paste0("./_tempdata/polData.parquet"))
 gc()
-
-# Export files to parquet -------------------------------------------------
-# Create list of datasets
-df_list <- names(which(unlist(eapply(
-  .GlobalEnv, is.data.frame
-))))
-
-# Export to parquet
-for (i in df_list) {
-  write_parquet(x = get(i),
-                sink = paste0("./_tempdata/", i, ".parquet"))
-  
-}
